0766c2de3da9a01a8f9ab86bb86bd1cdcd087095
rayhan <rayhan.a.ahmed@accenture.com>
2023-04-16 16:39:06 +0100

    updated the test

--- src/main/java/thirdparty/paymentgateway/TicketExercise.java
+++ src/main/java/thirdparty/paymentgateway/TicketExercise.java
@@ -13,92 +13,111 @@ public class TicketExercise {
 
     public static void main(String[] args) {
 
-// AdultTicket class
+        // AdultTicket class
         class AdultTicket {
             private int price;
+
             public AdultTicket() {
                 this.price = 20; // price for adult ticket is 20
             }
+
             public int getPrice() {
                 return price;
             }
 
         }
 
-
-// InfantTicket class
-
+        // InfantTicket class
         class InfantTicket {
             private int price;
+
             public InfantTicket() {
                 this.price = 0; // price for infant ticket is 0
             }
+
             public int getPrice() {
                 return price;
             }
 
         }
 
-// ChildTicket class
-
+        // ChildTicket class
         class ChildTicket {
             private int price;
+
             public ChildTicket() {
                 this.price = 10; // price for child ticket is 10
             }
+
             public int getPrice() {
                 return price;
             }
         }
 
-
-//creating a scanner object to read user input
+        // Creating a scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-//entering prompt for number of tickets user wants to purchase
-        System.out.println("how much do you want to buy");
-//reading the user input
+
+        // Prompting the user for the number of tickets they want to purchase
+        System.out.println("How many tickets do you want to buy?");
+
+        // Reading the user input
         int numberOfTickets = scanner.nextInt();
-//Checking if number of tickets meet the 20 ticket rule
+
+        // Checking if the number of tickets meets the 20 ticket rule
         if (numberOfTickets <= 0 || numberOfTickets > 20) {
-            System.out.println("Sorry. Only maximum 20 tickets can be purchased at a time");
+            System.out.println("Sorry. Only a maximum of 20 tickets can be purchased at a time");
             return;
         }
 
-//entering prompt for number of infants attending
-        System.out.println("How many infants will be attending?");
-//reading the user input
-        int numberOfInfants = scanner.nextInt();
-//entering prompt for number of Adults attending
-        System.out.println("How many Adults will be attending?");
-//reading the user input
+
+        // Prompting the user for the number of adults attending
+        System.out.println("How many adults will be attending?");
+
+        // Reading the user input
         int numberOfAdults = scanner.nextInt();
-//Checking if the number of adults is atleast equal to number of infants entered
+
+        // Checking if there is at least one adult to purchase tickets
+        if (numberOfAdults <= 0) {
+            System.out.println("Sorry, at least one adult is required to purchase tickets.");
+            return;
+        }
+
+        // Prompting the user for the number of infants attending
+        System.out.println("How many infants will be attending?");
+
+        // Reading the user input
+        int numberOfInfants = scanner.nextInt();
+
+        // Checking if the number of adults is at least equal to the number of infants entered
         if (!(numberOfInfants <= numberOfAdults)) {
             System.out.println("Sorry. You don't have enough adults with you");
             return;
         }
 
-//entering prompt for number of children attending
-        System.out.println("How many childs will be attending?");
-//reading the user input
-        int numberOfChilds = scanner.nextInt();
-//Making sure the number of tickets is equal to amount attending
-        if (!(numberOfTickets == numberOfAdults + numberOfChilds + numberOfInfants)) {
-            System.out.println("Sorry. You are buying more than " + numberOfTickets + " tickets");
+
+        // Prompting the user for the number of children attending
+        System.out.println("How many children will be attending?");
+
+        // Reading the user input
+        int numberOfChildren = scanner.nextInt();
+
+        // Making sure the number of tickets is equal to the number of people attending
+        if (( numberOfAdults + numberOfChildren + numberOfInfants > 20)) {
+            System.out.println("Sorry. You are buying more than 20 tickets");
             return;
         }
 
-// displaying to the user how much they have requested
-        System.out.println("You have requested " + numberOfInfants + " Infant tickets, " + numberOfChilds + " Child tickets and " + numberOfAdults + " Adult tickets");
+        // Displaying to the user how many tickets they have requested
+        System.out.println("You have requested " + numberOfInfants + " infant tickets, " + numberOfChildren + " child tickets, and " + numberOfAdults + " adult tickets");
         System.out.println("");
 
-// Creating ticket objects
+        // Creating ticket objects
         InfantTicket infantTicket = new InfantTicket();
         ChildTicket childTicket = new ChildTicket();
         AdultTicket adultTicket = new AdultTicket();
         int totalCost = numberOfInfants * infantTicket.getPrice() +
                 numberOfAdults * adultTicket.getPrice() +
-                numberOfChilds * childTicket.getPrice();
+                numberOfChildren * childTicket.getPrice();
         System.out.println("Total cost of tickets will be: " + totalCost);
 
 
@@ -119,7 +138,7 @@ public class TicketExercise {
         }
 
 //now adding adult tickets and child tickets to see how many seats to reserve
-        int numberOfSeatsToReserve = numberOfAdults + numberOfChilds;
+        int numberOfSeatsToReserve = numberOfAdults + numberOfChildren;
         System.out.println("We will look to reserve for you: " + numberOfSeatsToReserve + " seats");
 
 // Seat reservation request to SeatReservationService
@@ -143,12 +162,7 @@ public class TicketExercise {
 
     }
 
-    public void purchaseAndReserveTicket() {
-    }
 
-    public String getErrorMsg() {
-        return null;
-    }
 }
 
 
